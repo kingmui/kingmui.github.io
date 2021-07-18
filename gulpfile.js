c025ec6fd45c58090d7f515526710a4773c41118
kingmui <muikinghk@yahoo.com.hk>
2021-07-18 17:49:40 +0800

    chore: upgrade hexo-theme-next@8.6.1

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,27 @@
-let gulp = require('gulp'),
-    // imageminPngquant = require('imagemin-pngquant'),
-    $ = require('gulp-load-plugins')();
+const gulp = require("gulp");
+const $ = require("gulp-load-plugins")();
 
 // 压缩 html
-gulp.task('html', function () {
-    return gulp.src('./public/**/*.html')
-        .pipe($.htmlclean())
-        .pipe($.htmlmin({
-            collapseWhitespace: true, // 折叠有助于文档树中文本节点的空白区域
-            minifyCSS: true,
-            minifyJS: true,
-            minifyURLs: true, // 使用 relatedurl 压缩各种属性中的 URL
-            keepClosingSlash: true, // 单标签保留尾部斜杠
-            removeAttributeQuotes: true, // 尽可能删除属性周围的引号
-            removeEmptyAttributes: true, // 删除所有空格作属性值
-            removeComments: true, // 清除HTML注释
-            removeScriptTypeAttributes: true,
-            removeStyleLinkTypeAttributes: true,
-            useShortDoctype: true
-        }))
-        .pipe(gulp.dest('./public'));
+gulp.task("html", function () {
+  return gulp
+    .src("./public/**/*.html")
+    .pipe($.htmlclean())
+    .pipe(
+      $.htmlmin({
+        collapseWhitespace: true, // 折叠有助于文档树中文本节点的空白区域
+        minifyCSS: true,
+        minifyJS: true,
+        minifyURLs: true, // 使用 relatedurl 压缩各种属性中的 URL
+        keepClosingSlash: true, // 单标签保留尾部斜杠
+        removeAttributeQuotes: true, // 尽可能删除属性周围的引号
+        removeEmptyAttributes: true, // 删除所有空格作属性值
+        removeComments: true, // 清除HTML注释
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+      })
+    )
+    .pipe(gulp.dest("./public"));
 });
 
-// 压缩图片
-// gulp.task('image', () => {
-//     return gulp.src('./public/images/*')
-//         .pipe($.cache($.imagemin({
-//             interlaced: true,
-//             progressive: true,
-//             optimizationLevel: 5,
-//             svgoPlugins: [{ removeViewBox: true }],
-//             // 使用pngquant深度压缩png图片的imagemin插件
-//             use: [imageminPngquant()]
-//         })))
-//         .pipe(gulp.dest('./public/images/'))
-// });
-
-// 清除缓存
-// gulp.task('clearCache', () =>
-//     $.cache.clearAll()
-// );
-
-// 压缩 css
-// gulp.task('css', function() {
-//     return gulp.src('./public/**/*.css')
-//         .pipe($.cleanCss({compatibility: 'ie8', debug: true}, (details) => {
-//             console.log(`${details.name}: ${details.stats.originalSize}`);
-//             console.log(`${details.name}: ${details.stats.minifiedSize}`);
-//           }))
-//         .pipe(gulp.dest('./public'));
-// });
-
-// 压缩 js
-// gulp.task('js', function() {
-//     return gulp.src('./public/**/*.js')
-//         .pipe($.uglify())
-//         .pipe(gulp.dest('./public'));
-// });
-
-// 执行 gulp 命令时执行的任务
-gulp.task('default', [
-    'html',
-]);
+gulp.task("default", ["html"]);
---
+++ source/404/index.md
@@ -0,0 +1,8 @@
+---
+标题：'404'
+日期：2014-12-22 12:39:04
+评论：false 
+--- 
+< script  src = "//qzonestyle.gtimg.cn/qzone/hybrid/app/404/search_children.js "
+         charset="utf-8" homePageUrl="/" homePageName="返回首页"> 
+</ script >
